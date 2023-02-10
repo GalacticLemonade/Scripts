a96677d9b7f3fc11839321ec96acab32043df32e
GalacticLemonade <90120199+GalacticLemonade@users.noreply.github.com>
2023-02-10 10:11:30 -0700

    Update main.lua

--- Doors/main.lua
+++ Doors/main.lua
@@ -1,1 +1,11 @@
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
 
+local Window = Rayfield:CreateWindow({
+   Name = "Doors Hub",
+   LoadingTitle = "Doors Hub",
+   LoadingSubtitle = "by GalacticLemonade",
+})
+
+local Tab = Window:CreateTab("Main", 4483362458)
+
+local Label = Tab:CreateLabel("Sorry, but this is currently under construction. Check back later!")
