531599a958eb472eda77802a1e6c7607203dfe9e
GalacticLemonade <90120199+GalacticLemonade@users.noreply.github.com>
2023-02-10 19:58:40 -0700

    Create PowerThrow.lua

---
+++ FlingThings/PowerThrow.lua
@@ -0,0 +1,34 @@
+local bodyvel_Name = "FlingVelocity"
+local userinputs = game:GetService("UserInputService")
+local w = game:GetService("Workspace")
+local r = game:GetService("RunService")
+local d = game:GetService("Debris")
+local strength = _G.CurrentStrength
+local enabled = _G.strengthenabled
+ 
+w.ChildAdded:Connect(function(model)
+    if model.Name == "GrabParts" then
+        local part_to_impulse = model["GrabPart"]["WeldConstraint"].Part1
+ 
+        if part_to_impulse then
+ 
+            local inputObj
+            local velocityObj = Instance.new("BodyVelocity", part_to_impulse)
+            
+            model:GetPropertyChangedSignal("Parent"):Connect(function()
+                if not model.Parent and enabled == true then
+                    if userinputs:GetLastInputType() == Enum.UserInputType.MouseButton2 and enabled == true then
+                        print("Launched!")
+                        velocityObj.MaxForce = Vector3.new(math.huge, math.huge, math.huge)
+                        velocityObj.Velocity = workspace.CurrentCamera.CFrame.lookVector * strength
+                        d:AddItem(velocityObj, 1)
+                    elseif userinputs:GetLastInputType() == Enum.UserInputType.MouseButton1 then
+                        velocityObj:Destroy()
+                    else
+                        velocityObj:Destroy()
+                    end
+                end
+            end)
+        end
+    end
+end)
