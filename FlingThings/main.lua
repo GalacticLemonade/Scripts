14ff24e5a88fdc9d91a5f8ff5201da0e7ed004b7
GalacticLemonade <90120199+GalacticLemonade@users.noreply.github.com>
2023-02-11 14:14:42 -0700

    Update main.lua

--- FlingThings/main.lua
+++ FlingThings/main.lua
@@ -1,2 +1,20 @@
 local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
 
+local AntiFling = loadstring(game:HttpGet('https://raw.githubusercontent.com/GalacticLemonade/Scripts/main/FlingThings/AntiGrab.lua'))()
+local ThrowStrength = loadstring(game:HttpGet('https://raw.githubusercontent.com/GalacticLemonade/Scripts/main/FlingThings/PowerThrow.lua'))()
+
+function DestroyUI()
+  AntiFling:Destroy()
+  ThrowStrength:Destroy()
+  Rayfield:Destroy()
+  script:Destroy()
+end
+
+local Window = Rayfield:CreateWindow({
+   Name = "Fling Things and People",
+   LoadingTitle = "Fling Things and People Hub",
+   LoadingSubtitle = "by GalacticLemonade",
+})
+
+local Tab = Window:CreateTab("Main", 4483362458) -- Title, Image
+
