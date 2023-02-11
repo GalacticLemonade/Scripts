986710adaecbad35354e5e0ee781da27ce591bcb
GalacticLemonade <90120199+GalacticLemonade@users.noreply.github.com>
2023-02-11 14:19:29 -0700

    Update main.lua

--- FlingThings/main.lua
+++ FlingThings/main.lua
@@ -18,3 +18,30 @@ local Window = Rayfield:CreateWindow({
 
 local Tab = Window:CreateTab("Main", 4483362458) -- Title, Image
 
+local SuperStrength = Tab:CreateToggle({
+   Name = "Super Strength Toggle",
+   CurrentValue = false,
+   Flag = "Toggle1",
+   Callback = function(Value)
+     getgenv().strengthenabled = Value
+   end,
+})
+
+local SuperStrengthSlider = Tab:CreateSlider({
+   Name = "Strength Power",
+   Range = {0, 500},
+   Increment = 10,
+   Suffix = "Strength",
+   CurrentValue = 1,
+   Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Value)
+      getgenv().CurrentStrength = tonumber(Value)
+   end,
+})
+
+local DeleteUI = Tab:CreateButton({
+   Name = "Destroy UI",
+   Callback = function()
+      DestroyUI()
+   end,
+})
