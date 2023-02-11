b605f6300f08b1c6af9bc93dbd2a8ba84d437627
GalacticLemonade <90120199+GalacticLemonade@users.noreply.github.com>
2023-02-10 19:14:31 -0700

    Update main.lua

--- FlingThings/main.lua
+++ FlingThings/main.lua
@@ -25,7 +25,7 @@ local AntiGrabButton  = Tab:CreateButton({
 
 local StrengthSlider = Tab:CreateSlider({
    Name = "Throw strength",
-   Range = {0, 20000},
+   Range = {0, 10000},
    Increment = 10,
    Suffix = "Strength",
    CurrentValue = 1,
