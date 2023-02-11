56ad5c3916d11171c1f60cbdc5a6dbebff69b6e1
GalacticLemonade <90120199+GalacticLemonade@users.noreply.github.com>
2023-02-11 14:47:58 -0700

    Update PowerThrow.lua

--- FlingThings/PowerThrow.lua
+++ FlingThings/PowerThrow.lua
@@ -4,32 +4,31 @@ local w = game:GetService("Workspace")
 local r = game:GetService("RunService")
 local d = game:GetService("Debris")
 
-local strength = getgenv().CurrentStrength
-local enabled = getgenv().strengthenabled
+while task.wait(2) do
+ local strength = getgenv().CurrentStrength
+ w.ChildAdded:Connect(function(model)
+   if model.Name == "GrabParts" then
+    local part_to_impulse = model["GrabPart"]["WeldConstraint"].Part1
  
-w.ChildAdded:Connect(function(model)
-    if model.Name == "GrabParts" then
-        local part_to_impulse = model["GrabPart"]["WeldConstraint"].Part1
+    if part_to_impulse then
  
-        if part_to_impulse then
- 
-            local inputObj
-            local velocityObj = Instance.new("BodyVelocity", part_to_impulse)
-            
-            model:GetPropertyChangedSignal("Parent"):Connect(function()
-                if not model.Parent and enabled == true then
-                    if userinputs:GetLastInputType() == Enum.UserInputType.MouseButton2 and enabled == true then
-                        print("Launched!")
-                        velocityObj.MaxForce = Vector3.new(math.huge, math.huge, math.huge)
-                        velocityObj.Velocity = workspace.CurrentCamera.CFrame.lookVector * strength
-                        d:AddItem(velocityObj, 1)
-                    elseif userinputs:GetLastInputType() == Enum.UserInputType.MouseButton1 then
-                        velocityObj:Destroy()
-                    else
-                        velocityObj:Destroy()
-                    end
-                end
-            end)
+     local inputObj
+     local velocityObj = Instance.new("BodyVelocity", part_to_impulse)
+     
+     model:GetPropertyChangedSignal("Parent"):Connect(function()
+       if not model.Parent then
+        if userinputs:GetLastInputType() == Enum.UserInputType.MouseButton2 then
+         velocityObj.MaxForce = Vector3.new(math.huge, math.huge, math.huge)
+         velocityObj.Velocity = workspace.CurrentCamera.CFrame.lookVector * strength
+         d:AddItem(velocityObj, 1)
+        elseif userinputs:GetLastInputType() == Enum.UserInputType.MouseButton1 then
+         velocityObj:Destroy()
+        else
+         velocityObj:Destroy()
         end
+       end
+      end)
     end
-end)
+   end
+  end)
+end
